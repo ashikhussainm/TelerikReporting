ef4d18a07b11cfaf8e77af8a25870b89eca5199c
Ashik M Hussain <ashikmhussain.a@gmail.com>
2025-05-19 14:07:34 +0530

    Report Engine Working in Local Code Push

---
+++ ReportingEngine/ReportingEngine/Models/RequestModels/InvoiceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ReportingEngine.Models.RequestModels
+{
+    public class InvoiceRequest
+    {
+        public string InvoiceNumber { get; set; }
+
+        public string ReportName { get; set; }
+
+        public string ReportExportType { get; set; }
+    }
+}
